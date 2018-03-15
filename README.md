74a2dee946236c9661f6ca24e275e5535b8c8f65
lancebeet <jw@31-208-45-132.cust.bredband2.com>
2018-03-15 13:09:09 +0100

    svg table

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # julia-scripts
 various julia scripts
+
+## periodictabletoy.jl
+<img src="demo.png" style="width: 500px;"/>
+
--- periodictabletoy.jl
+++ periodictabletoy.jl
@@ -1,6 +1,52 @@
 using PeriodicTable, InteractNext, Blink
 
 pt = PeriodicTable.PT()
+coordinates = [(el.data["xpos"],el.data["ypos"]) for el in pt.data]
+const width  = 300
+const height = 180
+const pad = 5
+const scale  = Int(floor(2/3*min((height-pad)/10, (width-pad)/18)))
+const dist   = Int(floor(scale/2))
+
+
+const colors = Dict(
+              "diatomic nonmetal"     => "khaki",
+              "noble gas"             => "aqua",
+              "alkali metal"          => "crimson",
+              "alkaline earth metal"  => "orange",
+              "metalloid"             => "brown",
+              "polyatomic nonmetal"   => "lightgreen",
+              "post-transition metal" => "gray",
+              "transition metal"      => "salmon",
+              "lanthanide"            => "pink",
+              "actinide"              => "purple")
+
+function color(i)
+    if i in keys(colors)
+        return colors[i]
+    else
+        return "lightgray"
+    end
+end
+function draw_table(pt, i)
+    getx(el) = el.data["xpos"]
+    gety(el) = el.data["ypos"]
+    getcat(el) = el.data["category"]
+    elm = pt.data[i]
+    dom"svg:svg[height=$height, width=$width]"(
+                      dom"svg:rect[width = $(scale+2), height = $(scale+2), 
+                      x=$(getx(elm)*(scale+dist)-1), 
+                      y=$(gety(elm)*(scale+dist)-1),
+                      strokeWidth=3,
+                      stroke=black]"(),
+                 (dom"svg:rect[width = $scale, height = $scale, 
+                      x=$(getx(el)*(scale+dist)), 
+                      y=$(gety(el)*(scale+dist)),
+                      fill=$(color(getcat(el)))]"()
+                      for el in pt.data)...
+                     )
+end
+  
 ui = @manipulate for elementno in 1:119
     el = pt.data[elementno].data
 
@@ -8,7 +54,8 @@ ui = @manipulate for elementno in 1:119
     #Wikipedia hotlinking api
     imgdomain = "https://en.wikipedia.org/wiki/Special:Redirect/file/$file"
 
-    attr = Dict("src" => imgdomain)
+    attr = Dict("src" => imgdomain,
+               "width" => "500")
     spectrum_node = Node(:div, Node(:br), "Spectrum:", Node(:br), Node(:img, attributes=attr))
 
     if el["spectral_img"] == ""
@@ -17,6 +64,7 @@ ui = @manipulate for elementno in 1:119
 
     Node(:div, 
          Node(:br),
+         draw_table(pt, elementno),
          Node(:div, "$(el["name"]), $(el["symbol"])"),
          Node(:div, "$(el["category"])"),
          Node(:br),
