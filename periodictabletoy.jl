65fe9a85c69dbf29c281cfe3424dbc51c270932c
lancebeet <jw@31-208-45-132.cust.bredband2.com>
2018-03-16 12:43:09 +0100

    base show working

--- periodictabletoy.jl
+++ periodictabletoy.jl
@@ -42,37 +42,11 @@ function draw_table(i)
 end
   
 ui = @manipulate for elementno in 1:119
-    el = elements[elementno]
-
-    file = replace(el.spectral_img, "https://en.wikipedia.org/wiki/File:", "")
-    #Wikipedia hotlinking api
-    imgdomain = "https://en.wikipedia.org/wiki/Special:Redirect/file/$file"
-
-    attr = Dict("src" => imgdomain,
-               "width" => "500")
-    spectrum_node = Node(:div, Node(:br), "Spectrum:", Node(:br), Node(:img, attributes=attr))
-
-    if el.spectral_img == ""
-        spectrum_node = Node(:div)
-    end
-
     Node(:div, 
-         Node(:br),
          draw_table(elementno),
-         Node(:div, "$(el.name), $(el.symbol)"),
-         Node(:div, "$(el.category)"),
-         Node(:br),
-         Node(:div, "Summary: $(el.summary)"),
-         Node(:br),
-         Node(:div, "Atomic mass: $(el.atomic_mass)"),
-         Node(:div, "Density: $(el.density) g/cm³"),
-         Node(:div, "Melting point: $(el.melt)°C"),
-         Node(:div, "Boiling point: $(el.boil)°C"),
-         spectrum_node
-    
+         elements[elementno]
         )
 end
-
 
 w = Window()
 body!(w, ui)
