84f78bb2e6a023382a344117c477efaa5778da4a
lancebeet <lancebeet@users.noreply.github.com>
2018-03-20 09:22:48 +0100

    canvaswebio test

--- periodictabletoy.jl
+++ periodictabletoy.jl
@@ -1,8 +1,8 @@
-using PeriodicTable, InteractNext, Blink
+using PeriodicTable, InteractNext, Blink, CanvasWebIO
 
-const width  = 300
-const height = 180
-const pad = 5
+const width  = 500
+const height = 300
+const pad = max(width,height)/15
 const scale  = Int(floor(2/3*min((height-pad)/10, (width-pad)/18)))
 const dist   = Int(floor(scale/2))
 
@@ -26,27 +26,23 @@ function color(i)
         return "lightgray"
     end
 end
-function draw_table(i)
-    dom"svg:svg[height=$height, width=$width]"(
-                      dom"svg:rect[width = $(scale+2), height = $(scale+2), 
-                      x=$(elements[i].xpos*(scale+dist)-1), 
-                      y=$(elements[i].ypos*(scale+dist)-1),
-                      strokeWidth=3,
-                      stroke=black]"(),
-                 (dom"svg:rect[width = $scale, height = $scale, 
-                  x=$(elements[j].xpos*(scale+dist)), 
-                  y=$(elements[j].ypos*(scale+dist)),
-                  fill=$(color(elements[j].category))]"()
-                  for j in 1:length(elements))...
-                     )
+function make_table()
+    canvas = Canvas([height,width])
+    for j in 1:length(elements)
+        addclickable!(canvas, dom"svg:rect[width = $scale, height = $scale,
+                      x=$(elements[j].xpos*(scale+dist)), 
+                      y=$(elements[j].ypos*(scale+dist)),
+                      fill=$(color(elements[j].category)),
+                      id=$(elements[j].name)]"())
+    end
+    canvas
 end
-  
+table = make_table()
 ui = @manipulate for elementno in 1:119
-    Node(:div, 
-         draw_table(elementno),
-         elements[elementno]
-        )
+    elements[elementno]
 end
-
+on(table.selection) do val
+    observe(elementno)[] = elements[val].number
+end
 w = Window()
-body!(w, ui)
+body!(w, Node(:div, table(), ui))
