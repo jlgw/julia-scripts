d80d8b226ccb67b50053780a02320b27f519f622
lancebeet <lancebeet@users.noreply.github.com>
2018-03-23 12:05:32 +0100

    slight improvements

--- ball.jl
+++ ball.jl
@@ -1,15 +1,18 @@
 using CanvasWebIO, WebIO, Observables, Mux
 
 global running = true
-iv = [50.0, 0.0]
-t  = 0.02
 
-ballr  = 10
-width  = 800
-height = 800
-acc = 10.0
-batwidth = 150
-batheight = 20
+iv         = [50.0, 0.0]
+t          = 0.02
+ballr      = 10
+width      = 800
+height     = 800
+acc        = 10.0
+batwidth   = 150
+batheight  = 20
+timeout    = 0.2
+bounciness = 0.90
+
 field = Canvas([width, height],true)
 
 ball = dom"svg:circle[id=ball,
@@ -26,25 +29,29 @@ bat = dom"svg:rect[id=bat,
 ballv = Observable(field.w, "ballv", iv)
 batv = Observable(field.w, "batv", [0.0, 0.0])
 batlast = [0.0,0.0]
+bounce = Observable(time())
 @async while running
     sleep(t)
     if (ballv[][1]<0 && field["ball"][][1]<ballr) || (ballv[][1]>0 && field["ball"][][1]>width-ballr)
-        ballv[] = [-ballv[][1], ballv[][2]]
+        ballv[] = [-bounciness*ballv[][1], ballv[][2]]
     end
     if (ballv[][2]<0 && field["ball"][][2]<ballr) || (ballv[][2]>0 && field["ball"][][2]>height-ballr)
-        ballv[] = [ballv[][1], -ballv[][2]]
+        ballv[] = [ballv[][1], -bounciness*ballv[][2]]
     end
     batv[] = (field["bat"][]-batlast)/t
     batlast .= field["bat"][]
     disty = field["ball"][][2]-field["bat"][][2]-batheight/2
     distx = field["ball"][][1]-field["bat"][][1]-batwidth/2
-    if abs(distx)<batwidth/2 && abs(disty)<10+abs(2*t*ballv[][2]-2*t*batv[][2])
-        if sign(ballv[][2])!=sign(disty)
-            ballv[] = [ballv[][1], -ballv[][2]]+t*3*batv[]
+    if abs(distx)<batwidth/2 && abs(disty)<batheight/2+abs(1*t*ballv[][2]-1*t*batv[][2])
+        if sign(ballv[][2])!=sign(disty) && (time()-bounce[])>timeout
+            bounce[] = time()
+            ballv[] = [ballv[][1], -0.6*ballv[][2]]+0.5*batv[]
+        else
+            ballv[] = ballv[]+0.2*batv[]
         end
     end
+    field["ball"] = field["ball"][] + ballv[]*t
     ballv[] = ballv[] + [0.0, acc]
-    field["ball"] = field["ball"][] + ballv[].*[5.0,1.0]*t
 end
 
 addmovable!(field, ball)
